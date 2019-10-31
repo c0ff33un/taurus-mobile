80180f6f75d8e35b612085fcb7ddc6cfbf889145
Iván Andrés Lemus <ialemusm@unal.edu.co>
2019-10-31 03:59:16 -0500

    button added

--- src/TaurusApp.js
+++ src/TaurusApp.js
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   statusBar: {
-    backgroundColor: "green",
+    backgroundColor: "transparent",
     height: getStatusBarHeight()
   }
 });
